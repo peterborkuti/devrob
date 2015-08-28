0336b9291a4922e3552abb9e39bc9a7f5bfa3ca8
Peter Borkuti <peter.borkuti@liferay.com>
2015-08-28 15:57:02 +0200

    try

--- mymooc/DevRob01/src/hu/bp/pattern/Pattern.java
+++ mymooc/DevRob01/src/hu/bp/pattern/Pattern.java
@@ -1,6 +1,20 @@
 package hu.bp.pattern;
 
 public class Pattern {
+	public String getPattern() {
+		return pattern;
+	}
+
+	public int getOccurences() {
+		return occurences;
+	}
+
+	public Pattern(String pattern, int occurences) {
+		super();
+		this.pattern = pattern;
+		this.occurences = occurences;
+	}
+
 	private String pattern;
 	private int occurences;
 	
